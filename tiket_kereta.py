51529b4150b4472cb585ffca3045fd887a5d4a2b
DeaAnandaZ <66169255+DeaAnandaZ@users.noreply.github.com>
2020-06-01 14:51:24 +0700

    tambah tujuan 1

--- tiket_kereta.py
+++ tiket_kereta.py
@@ -16,3 +16,23 @@ while (True):
             for n in range (3):
                 print(n+1,Tujuan[n])
             tujuan=input("Pilih tujuan: ")
+            if tujuan== "1":
+                print("")
+                print("Kelas:")
+                Kelas=["Ekonomi","Bisnis"]
+                for x in range (2):
+                    print(x+1, Kelas[x])
+                kelas=input("Pilih kelas: ")
+                if kelas=="1":
+                    hari=input("Apakah weekend? (y/n): ")
+                    if hari=="y":
+                        print("Tiket kereta = Rp. 2000")
+                    else:
+                        print("Tiket kereta = Rp. 1000")
+               else:
+                hari=input("Apakah weekend? (y/n): ")
+                if hari=="y":
+                    print("Tiket kereta = Rp. 4000")
+                else:
+                    print("Tiket kereta = Rp. 3000")
+                    
